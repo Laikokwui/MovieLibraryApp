623fdeefb9c639db7cb58e8802f63a56ae023d12
Lai Kok Wui <47911209+Laikokwui@users.noreply.github.com>
2024-09-16 17:09:34 +0800

    optimise the code for mainactivity2

--- app/src/main/java/com/example/movieappextension/MainActivity2.java
+++ app/src/main/java/com/example/movieappextension/MainActivity2.java
@@ -18,11 +18,11 @@ public class MainActivity2 extends AppCompatActivity {
     public static final String DATA_KEY = "json";
     public static final String CLICKABLE_OPTION = "clickable";
 
-    private TextView textView_movie_title;
-    private TextView textView_popular;
-    private TextView textView_overviews;
-    private ImageView imageView_movie_banner;
-    private Button button_add_to_list;
+    private TextView textViewMovieTitle;
+    private TextView textViewPopularity;
+    private TextView textViewOverview;
+    private ImageView imageViewMovieBanner;
+    private Button buttonAddToList;
     private Movie movie;
 
     @Override
@@ -30,50 +30,56 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        textView_movie_title = findViewById(R.id.textView_movie_title);
-        textView_popular = findViewById(R.id.textView_popular);
-        textView_overviews = findViewById(R.id.textView_overviews);
-        imageView_movie_banner = findViewById(R.id.imageView_movie_banner);
-        button_add_to_list = findViewById(R.id.button_add_to_watch_list);
+        // Initialize UI elements
+        textViewMovieTitle = findViewById(R.id.textView_movie_title);
+        textViewPopularity = findViewById(R.id.textView_popular);
+        textViewOverview = findViewById(R.id.textView_overviews);
+        imageViewMovieBanner = findViewById(R.id.imageView_movie_banner);
+        buttonAddToList = findViewById(R.id.button_add_to_watch_list);
 
+        // Populate the UI with movie data if available
         if (getIntent().hasExtra(DATA_KEY)) {
             movie = getIntent().getParcelableExtra(DATA_KEY);
-            textView_movie_title.setText(movie.getTitle());
-            textView_popular.setText(String.valueOf(movie.getPopularity()));
-            textView_overviews.setText(movie.getOverview());
-            setMovieBanner("https://image.tmdb.org/t/p/w1280"+movie.getBackdrop_path());
+            if (movie != null) {
+                textViewMovieTitle.setText(movie.getTitle());
+                textViewPopularity.setText(String.valueOf(movie.getPopularity()));
+                textViewOverview.setText(movie.getOverview());
+                setMovieBanner("https://image.tmdb.org/t/p/w1280" + movie.getBackdrop_path());
+            }
         }
 
-        if (getIntent().getBooleanExtra(CLICKABLE_OPTION,false)) {
-            button_add_to_list.setVisibility(View.GONE);
-        }
-        else {
-            button_add_to_list.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent();
-                    Bundle bundle = new Bundle();
-                    bundle.putParcelable(MainActivity.INTENT_KEY_ADD,movie);
-                    intent.putExtras(bundle);
-                    setResult(RESULT_OK,intent);
-                    finish();
-                }
+        // Toggle the visibility of the "Add to Watchlist" button
+        if (getIntent().getBooleanExtra(CLICKABLE_OPTION, false)) {
+            buttonAddToList.setVisibility(View.GONE);
+        } else {
+            buttonAddToList.setOnClickListener(v -> {
+                Intent intent = new Intent();
+                intent.putExtra(MainActivity.INTENT_KEY_ADD, movie);
+                setResult(RESULT_OK, intent);
+                finish();
             });
         }
     }
 
+    // Method to set the movie banner image using the given URL
     private void setMovieBanner(String url) {
         ImageLoader imageLoader = Singleton.getInstance(getApplicationContext()).getImageLoader();
         imageLoader.get(url, new ImageLoader.ImageListener() {
             @Override
             public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
-                Bitmap bmp = response.getBitmap();
-                imageView_movie_banner.setImageBitmap(bmp);
+                Bitmap bitmap = response.getBitmap();
+                if (bitmap != null) {
+                    imageViewMovieBanner.setImageBitmap(bitmap);
+                } else {
+                    // Placeholder image or error handling if bitmap is null
+                    imageViewMovieBanner.setImageResource(R.drawable.placeholder_image); 
+                }
             }
 
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("Error load Movie Banner: ", error.getMessage());
+                Log.e("MovieBannerError", "Error loading movie banner: " + error.getMessage());
+                imageViewMovieBanner.setImageResource(R.drawable.error_image); // Set an error image
             }
         });
     }
